2e9c2e59418bbbec1c80856a3d89321bea6792d6
AK-8778232465 <113067573+AK-8778232465@users.noreply.github.com>
2022-12-29 15:57:50 +0530

    Ver 1.1

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Net.Mail;
 using System.Net;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
 
 namespace DataGrid
 {
